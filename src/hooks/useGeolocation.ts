10dc9cf8c0369cf7632c649ea6c1d7d13dd9f70d
David Runemalm <david.runemalm@gmail.com>
2025-03-29 02:38:15 +0100

    Try to fix issue.

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -194,6 +194,14 @@ export const useGeolocation = (autoDetect = true) => {
 
   const setManualLocation = async (cityName: string) => {
 
+    setState({
+      coords: undefined,
+      detectedLocationInfo: undefined,
+      loading: true,
+      error: null
+    });
+    setIsManualLocation(true);
+
     try {
       const response = await fetch(
         `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(cityName)}, Sweden&countrycodes=se&limit=1`
